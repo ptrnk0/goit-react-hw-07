00a06c721bafa1877888ae9d5de53c6809c75f30
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 14:56:03 +0200

    feat: implement search functionality in SearchBox component

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectFilter } from "../../redux/selectors";
+import { changeFilter } from "../../redux/filterSlice";
+
 const SearchBox = () => {
-	return <div>SearchBox</div>;
+	const filter = useSelector(selectFilter);
+	const dispatch = useDispatch();
+
+	const handleInput = (value) => {
+		dispatch(changeFilter(value));
+	};
+
+	return (
+		<input
+			type="text"
+			placeholder="Find contacts by name"
+			value={filter}
+			onInput={(evt) => handleInput(evt.target.value.trim())}
+		/>
+	);
 };
 
 export default SearchBox;
