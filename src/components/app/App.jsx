5506872c47605e1fa41d40629ad8c335138e27e6
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 12:34:25 +0200

    implement main App component with Redux integration and contact management features

---
+++ src/components/app/App.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import ContacForm from "../contacForm/ContacForm";
+import ContactsList from "../contactsList/ContactsList";
+import SearchBox from "../searchBox/SearchBox";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/operations";
+import { selectError, selectLoading } from "../../redux/selectors";
+
+const App = () => {
+	const dispatch = useDispatch();
+	const isLoading = useSelector(selectLoading);
+	const error = useSelector(selectError);
+
+	useEffect(() => {
+		dispatch(fetchContacts());
+	}, [dispatch]);
+
+	return (
+		<main>
+			<ContacForm />
+			<SearchBox />
+			{error && <p>Something went wrong.</p>}
+			{isLoading && <p>Loading...</p>}
+			<ContactsList />
+		</main>
+	);
+};
+
+export default App;
