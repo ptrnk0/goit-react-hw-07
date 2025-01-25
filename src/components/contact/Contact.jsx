0c70db253efd6ce6e498dd554bb17be78817db37
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 14:22:09 +0200

    add delete functionality for contacts in Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,18 @@
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/operations";
+
 const Contact = ({ contact }) => {
+	const dispatch = useDispatch();
+	const handleDelete = () => {
+		dispatch(deleteContact(contact.id));
+	};
 	return (
 		<>
 			<h3>{contact.name}</h3>
 			<p>{contact.number}</p>
+			<button type="button" onClick={handleDelete}>
+				Delete
+			</button>
 		</>
 	);
 };
