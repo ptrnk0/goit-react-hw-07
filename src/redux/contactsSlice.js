30b9df96e01aa710d106b91a63e7e7b65b914e2a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 14:22:48 +0200

    implement loading and error handling for add and delete contact operations in contactsSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchContacts } from "./operations";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { addContact, deleteContact, fetchContacts } from "./operations";
 
 const initialState = {
 	items: [],
@@ -12,18 +12,42 @@ const contactsSlice = createSlice({
 	initialState,
 	extraReducers: (builder) => {
 		builder
-			.addCase(fetchContacts.pending, (state) => {
-				state.loading = true;
-				state.error = null;
-			})
 			.addCase(fetchContacts.fulfilled, (state, action) => {
 				state.loading = false;
 				state.items = action.payload;
 			})
-			.addCase(fetchContacts.rejected, (state, action) => {
+			.addCase(addContact.fulfilled, (state, action) => {
 				state.loading = false;
-				state.error = action.payload;
-			});
+				state.items.push(action.payload);
+			})
+			.addCase(deleteContact.fulfilled, (state, action) => {
+				state.loading = false;
+				state.items = state.items.filter(
+					(contact) => contact.id !== action.payload.id
+				);
+			})
+			.addMatcher(
+				isAnyOf(
+					fetchContacts.pending,
+					addContact.pending,
+					deleteContact.pending
+				),
+				(state) => {
+					state.loading = true;
+					state.error = null;
+				}
+			)
+			.addMatcher(
+				isAnyOf(
+					fetchContacts.rejected,
+					addContact.rejected,
+					deleteContact.rejected
+				),
+				(state, action) => {
+					state.loading = false;
+					state.error = action.payload;
+				}
+			);
 	},
 });
 
