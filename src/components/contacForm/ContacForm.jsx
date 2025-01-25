6db0f291e4f4a2298ac9e97b669652ea47dea3e3
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 14:54:47 +0200

    fix: update import path for addContact in ContacForm component

--- src/components/contacForm/ContacForm.jsx
+++ src/components/contacForm/ContacForm.jsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
-import { addContact } from "../../redux/operations";
+import { addContact } from "../../redux/contactsOps";
 import { useDispatch } from "react-redux";
 
 const contactFormSchema = Yup.object().shape({
