334368da22d90b073c10fbdce350fc585f7c19cf
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 12:34:47 +0200

    add ContactsList component to display a list of contacts from Redux state

---
+++ src/components/contactsList/ContactsList.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/selectors";
+import Contact from "../contact/Contact";
+
+const ContactsList = () => {
+	const contacts = useSelector(selectContacts);
+	console.log(contacts);
+
+	return (
+		<ul>
+			{contacts &&
+				contacts.map((contact) => {
+					return (
+						<li key={contact.id}>
+							<Contact contact={contact} />
+						</li>
+					);
+				})}
+		</ul>
+	);
+};
+
+export default ContactsList;
