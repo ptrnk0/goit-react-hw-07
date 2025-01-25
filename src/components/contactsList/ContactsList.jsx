831381302e752800910fd8c6206cb1d5f3a0f0fc
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 14:55:48 +0200

    feat: implement filtered contacts selection

--- src/components/contactsList/ContactsList.jsx
+++ src/components/contactsList/ContactsList.jsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/selectors";
+import { selectFilteredContacts } from "../../redux/selectors";
 import Contact from "../contact/Contact";
 
 const ContactsList = () => {
-	const contacts = useSelector(selectContacts);
+	const contacts = useSelector(selectFilteredContacts);
 
 	return (
 		<ul>
