b10e062609d35014987bc6cf3fd3728b1fa7df89
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-25 14:54:53 +0200

    fix: update import path for deleteContact in Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/operations";
+import { deleteContact } from "../../redux/contactsOps";
 
 const Contact = ({ contact }) => {
 	const dispatch = useDispatch();
